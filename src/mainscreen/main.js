75f24ad8c7c0a9761370306d6aa873ebae6c129e
TasnadiAttila <70896019+TasnadiAttila@users.noreply.github.com>
2023-10-20 19:49:09 +0200

    Bug pages opens but you cant do nothing yet

--- src/mainscreen/main.js
+++ src/mainscreen/main.js
@@ -1,4 +1,25 @@
-function redirectToGame(){
+function redirectToGame(){ 
+    //TODO: needs to bring the person the character picker screen
     window.location.href = "../game/game.html";
 }
 document.getElementById('goToGame').addEventListener('click',redirectToGame);
+
+function redirectToScoreboard(){
+    window.location.href = "../scoreboard/scoreboard.html";
+}
+document.getElementById('goToScoreboard').addEventListener('click',redirectToScoreboard);
+
+function openBugReport(){
+    var url = "bug.html";
+    var screenWidth = window.screen.width;
+    var screenHeight = window.screen.height;
+    var windowWidth = 800;
+    var windowHeight = 600;
+    var left = (screenWidth - windowWidth) / 2;
+    var top = (screenHeight - windowHeight) / 2;
+    var windowName = "Bug Reports";
+    var windowFeatures = "width=" + windowWidth + ",height=" + windowHeight + ",left=" + left + ",top=" + top + ",toolbar=no,location=no,status=no,menubar=no,scrollbars=yes";
+    var newWindow = window.open(url, windowName, windowFeatures);
+}
+
+document.getElementById('goToBugreport').addEventListener('click',openBugReport);
