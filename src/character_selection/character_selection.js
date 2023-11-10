1fb565ee9f65422749dfe29ff312fc130d97c4bf
Kis Ábel <abel.kis45682@gmail.com>
2023-11-10 16:37:34 +0100

    karakter választó bug javítás és megjelenés update

--- src/character_selection/character_selection.js
+++ src/character_selection/character_selection.js
@@ -9,45 +9,60 @@ let player1_index = 0;
 let player2_index = 0;
 let map_index = 0;
 
+const player1_img = document.querySelector("img.player1");
+const player2_img = document.querySelector("img.player2");
+const player1_name = document.querySelector("input.player1");
+const player2_name = document.querySelector("input.player2");
+const map_img = document.querySelector("img.map");
 
-function onNameChanged(caller){
-    getSpriteByName(caller.value).then((skinData)=>{
-        if(skinData != undefined)
-        document.querySelector("img."+caller.classList[0]).src = skinData; 
-        //TODO: change player index to reflect the selected skin
-    });
+const startButton = document.querySelector("button.startGame");
+
+
+function loadPlayerSkins(){
+    if(isEmpty(player1_name.value) == false){
+        getSkinIDbyPlayerName(player1_name.value).then((skinId)=>{
+            if(skinId != undefined){
+                player1_img.src = skins.get(skinId);
+                player1_index = numbericSkins.indexOf(skinId);
+            }
+        });
+    }
+    if(isEmpty(player2_name.value) == false){
+        getSkinIDbyPlayerName(player2_name.value).then((skinId)=>{
+            if(skinId != undefined){
+                player2_img.src = skins.get(skinId);
+                player2_index = numbericSkins.indexOf(skinId);
+            }
+        });
+    }
 } 
 
-// Kinézetválasztó  gombok megnyomásakor hívódik meg
-document.querySelectorAll("button.skin").forEach((b)=>b.addEventListener("click",changeSkin));
-function changeSkin(event){
-    if(event.target.classList.contains("player1")){
-        if(event.target.classList.contains("left")){
+function changeSkin(player, direction){
+    if(player == "player1"){
+        if(direction == "left"){
             player1_index = (--player1_index+numbericSkins.length)%numbericSkins.length
         }else{
             player1_index =  (++player1_index+numbericSkins.length)%numbericSkins.length
         }
-        document.querySelector("img.player1").src = skins.get(numbericSkins[player1_index]);
+        player1_img.src = skins.get(numbericSkins[player1_index]);
     }else{
-        if(event.target.classList.contains("left")){
+        if(direction == "left"){
             player2_index = (--player2_index+numbericSkins.length)%numbericSkins.length
         }else{
             player2_index = (++player2_index+numbericSkins.length)%numbericSkins.length
         }
-        document.querySelector("img.player2").src = skins.get(numbericSkins[player2_index]);
+        player2_img.src = skins.get(numbericSkins[player2_index]);
     }
 
 }
 
-// Mapválasztó gombok megnyomásakor aktiválódik
-document.querySelectorAll("button.map").forEach((b)=>b.addEventListener("click",changeMap));
-function changeMap(event){
-    if(event.target.classList.contains("left")){
+function changeMap(direction){
+    if(direction == "left"){
         map_index = (--map_index+numbericMaps.length)%numbericMaps.length
     }else{
         map_index = (++map_index+numbericMaps.length)%numbericMaps.length
     }
-    document.querySelector("img.map").src = maps.get(numbericMaps[map_index]);
+    map_img.src = maps.get(numbericMaps[map_index]);
 }
 
 
@@ -87,7 +102,7 @@ function loadAllMaps(){
     })
 }
 
-function getSpriteByName(playerName){
+function getSkinIDbyPlayerName(playerName){
     return new Promise((resolve, reject) => {
         $.ajax({
             url: '../php/getSkinIDbyPlayerName.php',
@@ -97,7 +112,7 @@ function getSpriteByName(playerName){
                 PlayerName: playerName
             },
             success: function(skinId) {
-                resolve(skins.get(skinId));
+                resolve(skinId);
             },
             error: function(jqXHR, textStatus, errorThrown) {
                 console.error('Error:', errorThrown);
@@ -106,32 +121,24 @@ function getSpriteByName(playerName){
     });
 }
 
-function start(){
+function startGame(){
     const toGame = {
-        player1_name: document.querySelector("input.player1").value,
-        player1_skindData: skins.get(numbericSkins[player1_index]),
-        player2_name: document.querySelector("input.player2").value,
-        player2_skindData:  skins.get(numbericSkins[player2_index]),
+        player1_name: player1_name.value,
+        player1_skindData: player1_img.src,
+        player2_name: player2_name.value,
+        player2_skindData: player2_img.src,
         mapData: maps.get(numbericMaps[map_index])
-    }
-    if(/^\s*$/.test(toGame.player1_name) || /^\s*$/.test(toGame.player2_name)){
-        alert("A név megadása kötelező!");
-        return;
-    }else if(toGame.player1_name == toGame.player2_name){
-        alert("Magad ellen tök béna küzdeni...")
-        return;
     }
 
     sessionStorage.setItem("gameDatas", JSON.stringify(toGame));
     sendPlayerDataToDatabase();
-    
 }
 
 function sendPlayerDataToDatabase(){
     const toDatabase = {
-        player1_name: document.querySelector("input.player1").value,
+        player1_name: player1_name.value,
         player1_skindId: numbericSkins[player1_index],
-        player2_name:  document.querySelector("input.player2").value,
+        player2_name: player2_name.value,
         player2_skindId: numbericSkins[player2_index]
     };
       
@@ -155,8 +162,13 @@ function sendPlayerDataToDatabase(){
     })
     .catch(error => {
         console.error('Error:', error.message);
-        alert("Valami nem jó...");
     });
 }
 
-document.getElementById("go").addEventListener("click", start);
+function isEmpty(string){
+    return /^\s*$/.test(string);
+}
+
+function updateStart(){
+    startButton.disabled = isEmpty(player1_name.value)||isEmpty(player2_name.value)||numbericMaps[map_index]==undefined;
+}
