a9f75c813d1853b8ceeb9a1b5218e35a626ff316
TasnadiAttila <70896019+TasnadiAttila@users.noreply.github.com>
2023-11-13 13:33:16 +0100

    Update fillScoreboard.php

--- src/php/fillScoreboard.php
+++ src/php/fillScoreboard.php
@@ -37,7 +37,7 @@ $html .= '</ul>';
 $html .= '</div>';
 $html .= '<div class="topScoreSkinConatiner" hidden>';
 $html .= '<h1>Legtöbb pontall rendelkező játékos:</h1>';
-$html .= '<img id="highest" class="playerSkinWithHighestScore" src="../images/noPlayer.png"></img>';
+$html .= '<img id="highest" class="playerSkinWithHighestScore"> </img>';
 $html .= '</div>';
 $html .= '</div>';
 $html .= '<input class="searchBar" title="Nyomd le a szóköz billentyűt részletesebb keresésért." 
